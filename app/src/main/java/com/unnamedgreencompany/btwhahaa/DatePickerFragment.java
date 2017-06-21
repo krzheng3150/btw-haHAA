86a78dcc190cacc6ae0f1cb32919c345dfc5c1fd
Nivek <kevin_ruoyu_zheng@hotmail.com>
2017-06-21 06:19:16 -0400

    added date picker fragment and interaction with main activity

---
+++ app/src/main/java/com/unnamedgreencompany/btwhahaa/DatePickerFragment.java
@@ -0,0 +1,118 @@
+package com.unnamedgreencompany.btwhahaa;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link DatePickerFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link DatePickerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String FIELD_ID_PARAM = "field_id";
+    private static final String FIELD_VALUE_PARAM = "field_value";
+
+    private int fieldId;
+    private String fieldValue;
+
+    private OnFragmentInteractionListener mListener;
+
+    public DatePickerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c;
+        try {
+            Date d = DateFormat.getDateFormat(getActivity()).parse(fieldValue);
+            c = Calendar.getInstance();
+            c.setTime(d);
+        }
+        catch (Exception e) {
+            // Use the current date as the default date in the picker
+            c = Calendar.getInstance();
+        }
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        mListener.onDateSelection(fieldId, year, month, day);
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param fieldId The ID of the field to edit.
+     * @param fieldValue The current fieldValue of that fieldId.
+     * @return A new instance of fragment DatePickerFragment.
+     */
+    public static DatePickerFragment newInstance(int fieldId, String fieldValue) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        Bundle args = new Bundle();
+        args.putInt(FIELD_ID_PARAM, fieldId);
+        args.putString(FIELD_VALUE_PARAM, fieldValue);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            fieldId = getArguments().getInt(FIELD_ID_PARAM);
+            fieldValue = getArguments().getString(FIELD_VALUE_PARAM);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onDateSelection(int fieldId, int year, int month, int day);
+    }
+}
--- app/src/main/java/com/unnamedgreencompany/btwhahaa/MainActivity.java
+++ app/src/main/java/com/unnamedgreencompany/btwhahaa/MainActivity.java
@@ -1,13 +1,57 @@
 package com.unnamedgreencompany.btwhahaa;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements DatePickerFragment.OnFragmentInteractionListener {
+
+    private java.text.DateFormat dateFormat;
+
+    private EditText birthdayEditor;
+    private EditText refDateEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateFormat = DateFormat.getDateFormat(this);
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+
+        final Date refDate = c.getTime();
+        c.add(Calendar.YEAR, -12);
+        final Date birthday = c.getTime();
+
+        birthdayEditor = (EditText)findViewById(R.id.birthday_editor);
+        birthdayEditor.setText(dateFormat.format(new Date(birthday.getTime())));
+        refDateEditor = (EditText)findViewById(R.id.ref_date_editor);
+        refDateEditor.setText(dateFormat.format(new Date(refDate.getTime())));
+    }
+
+    public void showDatePickerDialog(View v) {
+        String fieldName = v.getTag().toString();
+        int fieldId = getResources().getIdentifier(fieldName, "id", getPackageName());
+        EditText dateEditor = (EditText)findViewById(fieldId);
+        String value = dateEditor.getText().toString();
+        DialogFragment newFragment = DatePickerFragment.newInstance(fieldId, value);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onDateSelection(int fieldId, int year, int month, int day) {
+        EditText dateEditor = (EditText)findViewById(fieldId);
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, day);
+        dateEditor.setText(dateFormat.format(c.getTime()));
     }
 }

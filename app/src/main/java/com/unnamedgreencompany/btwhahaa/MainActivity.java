c759466d4079a32c865dbcc1a83597bc46eb3368
Nivek <kevin_ruoyu_zheng@hotmail.com>
2017-06-21 08:43:02 -0400

    added submit button and input validation

--- app/src/main/java/com/unnamedgreencompany/btwhahaa/MainActivity.java
+++ app/src/main/java/com/unnamedgreencompany/btwhahaa/MainActivity.java
@@ -1,11 +1,14 @@
 package com.unnamedgreencompany.btwhahaa;
 
+import android.graphics.Color;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
 
     private EditText birthdayEditor;
     private EditText refDateEditor;
+    private RadioGrid functions;
+
+    private static final long ONE_YEAR = 31536000000L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
         birthdayEditor.setText(dateFormat.format(new Date(birthday.getTime())));
         refDateEditor = (EditText)findViewById(R.id.ref_date_editor);
         refDateEditor.setText(dateFormat.format(new Date(refDate.getTime())));
+
+        functions = (RadioGrid)findViewById(R.id.functions);
     }
 
     public void showDatePickerDialog(View v) {
@@ -47,11 +55,46 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+    public void submitInputs(View v) {
+        Calendar birthday, refDate;
+        try {
+            Date birthdayInput = dateFormat.parse(birthdayEditor.getText().toString());
+            Date refDateInput = dateFormat.parse(refDateEditor.getText().toString());
+            if (refDateInput.before(birthdayInput)) {
+                showError(R.string.negative_age_error);
+                return;
+            }
+            if (refDateInput.getTime() - birthdayInput.getTime() < ONE_YEAR) {
+                showError(R.string.young_age_error);
+                return;
+            }
+            birthday = Calendar.getInstance();
+            birthday.setTime(birthdayInput);
+            refDate = Calendar.getInstance();
+            refDate.setTime(refDateInput);
+        }
+        catch (Exception e) {
+            showError(R.string.date_parse_error);
+            return;
+        }
+        if (functions.getCheckedRadioButtonId() == -1) {
+            showError(R.string.missing_function_error);
+            //return;
+        }
+    }
+
     @Override
     public void onDateSelection(int fieldId, int year, int month, int day) {
         EditText dateEditor = (EditText)findViewById(fieldId);
         Calendar c = Calendar.getInstance();
         c.set(year, month, day);
         dateEditor.setText(dateFormat.format(c.getTime()));
+    }
+
+    private void showError(int messageId) {
+        Toast toast = Toast.makeText(getApplicationContext(), messageId, Toast.LENGTH_LONG);
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setBackgroundColor(Color.RED);
+        toast.show();
     }
 }
